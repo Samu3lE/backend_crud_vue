dc19c2dbb032b78c63f5237ce625290c56b997ca
Samuel Espinoza <samuelesponline@gmail.com>
2021-12-29 14:59:14 -0500

    test-22

--- index.php
+++ index.php
@@ -19,6 +19,109 @@
             $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+            $request = $_GET;
+
+if(isset($request["search"])){
+    $employees_sql= "SELECT * FROM employee WHERE id={$_GET['search']}";
+
+  $stmt = $pdo->prepare($employees_sql);
+  $stmt->execute();
+  $rowCount = $stmt->rowCount();
+  
+    if($rowCount > 0){
+        $employees = $stmt->fetch();
+        echo json_encode($employees);
+        exit();
+    }else{
+        echo json_encode(["success"=>0]);
+        http_response_code(404);
+    }
+}else if(isset($request["delete"])){
+    $employees_sql= "DELETE FROM employee WHERE id={$_GET['delete']}";
+    $stmt = $pdo->prepare($employees_sql);
+    $success=$stmt->execute();
+    
+    if($success){
+        echo json_encode(["success"=>1]);
+        exit();
+    }else{
+        echo json_encode(["success"=>0]);
+        http_response_code(404);
+    }
+}else if(isset($request["create"])){
+    $data = json_decode(file_get_contents("php://input"));
+    $name = $data->name;
+    $email= $data->email;
+    if(($email!="")&&($name!="")){
+        $employees_sql= "INSERT INTO employee (name,email) VALUES ('{$name}','{$email}') ";
+
+        $stmt = $pdo->prepare($employees_sql);
+        $success=$stmt->execute();
+        
+        if($success){
+            echo json_encode(["success"=>1]);
+            
+        }else{
+                echo json_encode(["success"=>0]);
+                http_response_code(404);
+        }
+
+    }else{
+        echo json_encode(["success"=>0]);
+        http_response_code(404);
+     } exit();
+    
+}else if(isset($request["update"])){
+    $data = json_decode(file_get_contents("php://input"));
+    $id=(isset($data->id))?$data->id:$_GET["update"];
+    $name=$data->name;
+    $email=$data->email;
+    
+    if(($email!="")&&($name!="")){
+        $employees_sql= "UPDATE employee SET name='{$name}',email='{$email}' WHERE id='{$id}'";
+        $stmt = $pdo->prepare($employees_sql);
+        $success=$stmt->execute();
+        
+        if($success){
+            echo json_encode(["success"=>1]);
+            
+        }else{
+                echo json_encode(["success"=>0]);
+                http_response_code(404);
+        }
+    }else{
+        echo json_encode(["success"=>0]);
+        http_response_code(404);
+    }
+    exit();
+}else if(isset($request["list"])){
+        
+    $employees_sql= "SELECT * FROM employee";
+    
+    $stmt = $pdo->prepare($employees_sql);
+    $stmt->execute();
+    $rowCount = $stmt->rowCount();
+    
+      if($rowCount > 0){
+          $employees = $stmt->fetch();
+          echo json_encode($employees);
+          exit();
+      }else{
+        echo json_encode(["success"=>0]);
+        http_response_code(404);
+      }
+
+}else{
+    http_response_code(404);
+
+    echo json_encode([
+        "success" => 0,
+        "message" => 'no se encontro la ruta',
+    ]);
+
+    exit();
+}
+
     } catch (\Throwable $th) {
         echo $th;
     }
