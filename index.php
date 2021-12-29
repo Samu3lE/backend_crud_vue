57b7b6a65664a44378c5a9f4ef8b5bf81e4ca465
Samuel Espinoza <samuelesponline@gmail.com>
2021-12-29 12:37:58 -0500

    test-1

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
 <?php
+require("./vendor/autoload.php");
+
+$dotenv = Dotenv\Dotenv::createImmutable('./');
+        $dotenv->load();
+
+        $host = $_ENV['DB_HOST'];
+        $user=$_ENV['DB_USER'];
+        $password=$_ENV['DB_PASSWORD'];
+        $dbname=$_ENV['DB_NAME'];
+        $port=$_ENV['DB_PORT'];
+
+                //Set DSN data source name
+        $dsn = "pgsql:host=" . $host . ";port=" . $port .";dbname=" . $dbname . ";user=" . $user . ";password=" . $password . ";";
+
+        //         //create a pdo instance
+
+        $pdo = new PDO($dsn, $user, $password);
+  $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
+  $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+  
 echo "Hello there heroku, samuel Here!";
 ?>
