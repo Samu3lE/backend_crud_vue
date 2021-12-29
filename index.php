cf786051946147807f0bcf07e1b5ee42ef2957ba
Samuel Espinoza <samuelesponline@gmail.com>
2021-12-29 14:43:02 -0500

    test-14

--- index.php
+++ index.php
@@ -11,7 +11,17 @@
             $dbname=$_ENV['DB_NAME'];
             $port=$_ENV['DB_PORT'];
         
-        echo $host;
+                //Set DSN data source name
+                $dsn = "pgsql:host=" . $host . ";port=" . $port .";dbname=" . $dbname . ";user=" . $user . ";password=" . $password . ";";
+
+                //         //create a pdo instance
+        
+                $pdo = new PDO($dsn, $user, $password);
+          $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
+          $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
+          $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+          echo $pdo;
     } catch (\Throwable $th) {
         echo $th;
     }
