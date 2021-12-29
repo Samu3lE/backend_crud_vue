509d8048eb9cfe2066d7d989bc01faf42c4db16d
Samuel Espinoza <samuelesponline@gmail.com>
2021-12-28 19:16:34 -0500

    Refactory code

--- index.php
+++ index.php
@@ -1,25 +1,49 @@
 <?php
+
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: access");
 header("Access-Control-Allow-Methods: GET,POST");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-$server = "localhost";
-$user="root";
-$password="";
-$db_name="empleados";
+require("./vendor/autoload.php");
 
-$db_conection = new mysqli($server,$user,$password,$db_name);
+try{
+        $dotenv = Dotenv\Dotenv::createImmutable('./');
+        $dotenv->load();
+
+        $host = $_ENV['DB_HOST'];
+        $user=$_ENV['DB_USER'];
+        $password=$_ENV['DB_PASSWORD'];
+        $dbname=$_ENV['DB_NAME'];
+        $port=$_ENV['DB_PORT'];
+
+                //Set DSN data source name
+        $dsn = "pgsql:host=" . $host . ";port=" . $port .";dbname=" . $dbname . ";user=" . $user . ";password=" . $password . ";";
+
+        //         //create a pdo instance
+
+        $pdo = new PDO($dsn, $user, $password);
+  $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
+  $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 
+}
+catch (PDOException $e) {
+        echo 'Connection failed: ' . $e->getMessage();
+}
+   
 $request = $_GET;
 
 if(isset($request["search"])){
-    $employees_sql= mysqli_query($db_conection,"SELECT * FROM employee WHERE id={$_GET['search']}");
-    
-    if(mysqli_num_rows($employees_sql) > 0){
-        $employees = mysqli_fetch_all($employees_sql,MYSQLI_ASSOC);
+    $employees_sql= "SELECT * FROM employee WHERE id={$_GET['search']}";
+
+  $stmt = $pdo->prepare($employees_sql);
+  $stmt->execute();
+  $rowCount = $stmt->rowCount();
+    if($rowCount > 0){
+        $employees = $stmt->fetch();
         echo json_encode($employees);
         exit();
     }else{
