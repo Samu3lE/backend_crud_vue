bd0676f44602fa4e978718c87d479fe8137ab3f8
Samuel Espinoza <samuelesponline@gmail.com>
2021-12-29 12:40:51 -0500

    test-3

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <?php
 require("./vendor/autoload.php");
 
-$dotenv = Dotenv\Dotenv::createImmutable('./');
-        $dotenv->load();
-
         $host = $_ENV['DB_HOST'];
         $user=$_ENV['DB_USER'];
         $password=$_ENV['DB_PASSWORD'];
