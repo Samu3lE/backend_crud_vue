ec7f98935b44088665d1349f63e5d6066157f0fd
Samuel Espinoza <samuelesponline@gmail.com>
2021-12-29 12:38:51 -0500

    test-2

--- index.php
+++ index.php
@@ -9,17 +9,6 @@ $dotenv = Dotenv\Dotenv::createImmutable('./');
         $password=$_ENV['DB_PASSWORD'];
         $dbname=$_ENV['DB_NAME'];
         $port=$_ENV['DB_PORT'];
-
-                //Set DSN data source name
-        $dsn = "pgsql:host=" . $host . ";port=" . $port .";dbname=" . $dbname . ";user=" . $user . ";password=" . $password . ";";
-
-        //         //create a pdo instance
-
-        $pdo = new PDO($dsn, $user, $password);
-  $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
-  $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
-  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-  
+ 
 echo "Hello there heroku, samuel Here!";
 ?>
